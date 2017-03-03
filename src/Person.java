99a63447cbb62fe6b6dd7cb651b7a19f8d39b24d
EduardsKEA <edua0193@stud.kea.dk>
2017-03-03 13:52:05 +0100

    First commit and Person class create

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+/**
+ * Created by ArcticMonkey on 3/3/2017.
+ */
+public class Person {
+        int age;
+        String name;
+
+        public int getAge()
+        {
+            return -1;
+        }
+
+    public static void main(String[] args) {
+
+    }
+    }
